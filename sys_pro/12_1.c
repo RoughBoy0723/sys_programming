58ee8dd89bfa0c8a002be9d3273002bc3c97dd8e
RoughBoy <106831435+RoughBoy0723@users.noreply.github.com>
2023-05-31 21:24:56 +0900

    Delete sys_pro directory

--- sys_pro/12_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-int main(int argc, char *argv[]){
-    execl("/bin/cat", argv[1], (char *) 0);
-}
